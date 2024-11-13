1b9a0bfb54c724321703f6c4eeda34fb8a1091ae
n0escape200 <n0_escape200@yahoo.com>
2024-11-13 16:20:43 +0200

    Started working on the cloud storage for the photos

---
+++ VehicleApp/back/Controller/Cloud.js
@@ -0,0 +1,5 @@
+import {S3Client, ListBucketsCommand} from "@aws-sdk/client-s3"
+
+export const upload = async(req,res)=>{
+
+}
--- VehicleApp/front/src/Pages/PostCar/PostCar.tsx
+++ VehicleApp/front/src/Pages/PostCar/PostCar.tsx
@@ -4,6 +4,7 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus} from "@fortawesome/free-solid-svg-icons";
 import "./PostCar.css"
 import Navbar from "../../Components/Navbar/Navbar";
+import Cookies from "js-cookie";
 
 const PostCar = ()=>{
     const fileUploadRef = useRef()
@@ -36,24 +37,62 @@ const PostCar = ()=>{
 
     useEffect(() => {
         getCarBrands()
+        const cookie = Cookies.get("User");
+        if(cookie){
+            payload.owner = cookie;
+        }
     }, []);
 
-    const [uploadedFiles, uploadedFilesSet] = useState([])
+
+    const [photosUrl, photosUrlSet] = useState([])
+    const [photoUpload,photoUploadSet] = useState([])
 
     const handleFileChange = (event)=>{
-        //@ts-ignore
-        const files = Array.from(event.target.files)
-        const urlArray = files.map(file => ({
-            file,
-            url: URL.createObjectURL(file)
-        }))
-        uploadedFilesSet(prevState => [...prevState, ...urlArray])
+
+        const formData = new FormData();
+        const file = event.target.files[0]
+        formData.append(file.name,file)
+        formData.append("cloud_name","dvpfeykol")
+        formData.append("upload_preset","ml_default")
+        photoUploadSet(prevState => [...prevState,formData])
+
+        //Used to set array that will render the imgaes on screen
+        const url = { url: URL.createObjectURL(file)}
+        photosUrlSet(prevState => [...prevState,url])
     }
 
     useEffect(() => {
-        console.log(uploadedFiles)
-    }, [uploadedFiles]);
+        if(photoUpload){
+            console.log(photoUpload)
+        }
+    }, [photoUpload]);
 
+
+    const uploadData = ()=>{
+        photoUpload.map(async (file,_)=>{
+           await axios.post("https://api.cloudinary.com/v1_1/dvpfeykol/image/upload",file)
+               .then(response=>{
+                   console.log(response)})
+               .catch(error =>{
+                   console.log(error)})
+        })
+    }
+
+
+    const payload = {
+        brand:"",
+        model:"",
+        km:0,
+        cc:0,
+        year:0,
+        price:0,
+        currency:"EUR",
+        fuel:"Petrol",
+        condition:"New",
+        photos:[],
+        description:"",
+        owner:""
+    }
 
     return <div>
         <Navbar/>
@@ -77,6 +116,7 @@ const PostCar = ()=>{
                     if (modelsData != null) {
                         setModels(modelsData)
                     }
+                    payload.brand = event.target.value;
                 }}
                         style={{float: "right"}}>
                     <option>Select...</option>
@@ -89,7 +129,7 @@ const PostCar = ()=>{
             </div>
             <div>
                 <span>Model</span>
-                <select style={{float: "right"}}>
+                <select onChange={event => payload.model = event.target.value} style={{float: "right"}}>
                     <option>Select...</option>
                     {models && <>
                         {models.map((item, index) => {
@@ -100,30 +140,30 @@ const PostCar = ()=>{
             </div>
             <div>
                 <span>KM</span>
-                <input style={{float: "right"}}/>
+                <input onChange={event => payload.km = parseInt(event.target.value)} style={{float: "right"}}/>
             </div>
             <div>
                 <span>Cylinder capacity</span>
-                <input style={{float: "right"}}/>
+                <input onChange={event => payload.cc = parseInt(event.target.value)} style={{float: "right"}}/>
             </div>
             <div>
                 <span>Year</span>
-                <input style={{float: "right"}}/>
+                <input onChange={event => payload.year = parseInt(event.target.value)} style={{float: "right"}}/>
             </div>
             <div>
                 <span>Price</span>
-                <input style={{float: "right"}}/>
+                <input onChange={event => payload.price = parseInt(event.target.value)} style={{float: "right"}}/>
             </div>
             <div>
                 <span>Currency</span>
-                <select style={{float: "right"}}>
+                <select onChange={event => payload.currency = event.target.value} style={{float: "right"}}>
                     <option>EUR</option>
                     <option>USD</option>
                 </select>
             </div>
             <div>
                 <span>Fuel type</span>
-                <select style={{float: "right"}}>
+                <select onChange={event => payload.fuel = event.target.value} style={{float: "right"}}>
                     <option>Petrol</option>
                     <option>Diesel</option>
                     <option>Hybrid</option>
@@ -133,7 +173,7 @@ const PostCar = ()=>{
             </div>
             <div>
                 <span>Condition</span>
-                <select style={{float: "right"}}>
+                <select onChange={event => payload.condition = event.target.value} style={{float: "right"}}>
                     <option>New</option>
                     <option>Used</option>
                     <option>Non usable</option>
@@ -149,12 +189,12 @@ const PostCar = ()=>{
                     border: "1px solid black",
                     width: "fit-content",
                     padding:10
-                }}>{uploadedFiles.length == 0 ? <div onClick={() => {
+                }}>{photosUrl.length == 0 ? <div onClick={() => {
                         //@ts-ignore
                         fileUploadRef.current.click()
                     }} className={"add"}><FontAwesomeIcon icon={faPlus} size="2xl"/></div> :
                     <div style={{display:"flex", alignItems:"center", gap:30, flexWrap:"wrap"}}>
-                        {uploadedFiles.map((image, index) => {
+                        {photosUrl.map((image, index) => {
                             return <img style={{width: 150, height: "auto"}}
                                         src={image.url} alt="car" key={index}/>
                         })}
@@ -168,6 +208,8 @@ const PostCar = ()=>{
                 <span>Description</span>
                 <textarea style={{resize: "none"}} cols={30} rows={10}></textarea>
             </div>
+            <button onClick={uploadData}
+                style={{width:"fit-content", margin:"auto", marginTop:20, fontSize:32, padding:10, fontWeight:1000}}>Post car</button>
         </div>
     </div>
 }
